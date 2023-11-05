ad9f90bd007d79edcfdf9e13f83ea42a7f9c1d87
Y00L <skys1191@gmail.com>
2023-11-05 23:15:32 +0900

    이미지 적용
    이미지 적용
    가로 캔버스 안쓰는 식으로 수정
    해상도 맞춰 늘리는거 추가

--- Assets/CanvasComponent.cs
+++ Assets/CanvasComponent.cs
@@ -14,23 +14,37 @@ public class CanvasComponent : MonoBehaviour
     public  Image CatImage;
     public  Image AccImage;
     public GameObject[] ContentLayouts = new GameObject[3];
+    public Image[] MenuButtons = new Image[3];
     public int[] SelectedContentId = {0,0,0};
+    Color EnableColor = new(1,1,1,1);
+    Color DisableColor = new(1,1,1,0);
 
     // Start is called before the first frame update
     void Start()
     {
-       SetCategory(0);
+        SetCategory(0);
         for (int i = 0; i < ContentLayouts.Length; i++)
         {
-            ContentLayouts[i].transform.GetChild(0).gameObject.SetActive(false);
+            ContentLayouts[i].transform.GetChild(0).gameObject.GetComponent<Image>().color = EnableColor;
         }
     }
-    public void SetCategory(int id)
+    public void SetCategory(int enablingCategoryId)
     {
         //0: cat, 1: box, 2: acc
         for (int i = 0; i < ContentLayouts.Length; i++)
         {
-            ContentLayouts[i].SetActive(i == id);
+            ContentLayouts[i].SetActive(i == enablingCategoryId);
+
+            Sprite sprite;
+            if (i == enablingCategoryId)
+            {
+                sprite = gameManager.MenuButton_Selected[i];
+            }
+            else
+            {
+                sprite = gameManager.MenuButton_Deselected[i];
+            }
+            MenuButtons[i].sprite = sprite;
         }
     }
     public void SetContents_Cat(int id)
@@ -38,9 +52,9 @@ public class CanvasComponent : MonoBehaviour
         CatImage.sprite = gameManager.CatImages[id];
         
         int SelectedContentIndex = SelectedContentId[0];
-        ContentLayouts[0].transform.GetChild(SelectedContentIndex).gameObject.SetActive(true);
+        ContentLayouts[0].transform.GetChild(SelectedContentIndex).gameObject.GetComponent<Image>().color = DisableColor;
 
-        ContentLayouts[0].transform.GetChild(id).gameObject.SetActive(false);
+        ContentLayouts[0].transform.GetChild(id).gameObject.GetComponent<Image>().color = EnableColor;
         SelectedContentId[0] = id;
     }
     public void SetContents_Box(int id)
@@ -48,9 +62,9 @@ public class CanvasComponent : MonoBehaviour
         BoxImage.sprite = gameManager.BoxImages[id];
 
         int SelectedContentIndex = SelectedContentId[1];
-        ContentLayouts[1].transform.GetChild(SelectedContentIndex).gameObject.SetActive(true);
+        ContentLayouts[1].transform.GetChild(SelectedContentIndex).gameObject.GetComponent<Image>().color = DisableColor;
 
-        ContentLayouts[1].transform.GetChild(id).gameObject.SetActive(false);
+        ContentLayouts[1].transform.GetChild(id).gameObject.GetComponent<Image>().color = EnableColor;
         SelectedContentId[1] = id;
     }
     public void SetContents_Acc(int id)
@@ -58,9 +72,9 @@ public class CanvasComponent : MonoBehaviour
         AccImage.sprite = gameManager.AccImages[id];
 
         int SelectedContentIndex = SelectedContentId[2];
-        ContentLayouts[2].transform.GetChild(SelectedContentIndex).gameObject.SetActive(true);
+        ContentLayouts[2].transform.GetChild(SelectedContentIndex).gameObject.GetComponent<Image>().color = DisableColor;
 
-        ContentLayouts[2].transform.GetChild(id).gameObject.SetActive(false);
+        ContentLayouts[2].transform.GetChild(id).gameObject.GetComponent<Image>().color = EnableColor;
         SelectedContentId[2] = id;
     }
 }
--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -10,15 +10,33 @@ public class GameManager : MonoBehaviour
     public Sprite[] AccImages;
     public Sprite[] CatImages;
     public Sprite[] BoxImages;
+    public Sprite[] MenuButton_Selected;
+    public Sprite[] MenuButton_Deselected;
     
     // Start is called before the first frame update
     void Start()
     {
+        float standartRatio = (float)1920/(float)1080;
+        float screenRatio = (float)Screen.height / (float)Screen.width;
+        Debug.Log(standartRatio);
+        Debug.Log(screenRatio);
+        if (screenRatio>standartRatio)
+        {
+            PortraitCanvas.GetComponent<CanvasScaler>().matchWidthOrHeight = 0;
+        }
+        else
+        {
+            PortraitCanvas.GetComponent<CanvasScaler>().matchWidthOrHeight = 1;
+        }
+        PortraitCanvas.SetActive(true);
+
+        /*
         float ratio = (float)Screen.height / (float)Screen.width;
         Debug.Log(ratio);
         bool isPortaitDisplay = (ratio > 1.65);
         Debug.Log(isPortaitDisplay);
         ChangeCanvasByDisplayAspect(isPortaitDisplay);
+        */
     }
 
     // Update is called once per frame

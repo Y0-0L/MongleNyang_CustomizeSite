e7ca46478f4b924410a0de489ac47a60f4035339
Y00L <skys1191@gmail.com>
2024-03-15 14:29:31 +0900

    타이틀gif 제작용 빌드

--- Assets/Resource/UI/ObjectDisabler.cs
+++ Assets/Resource/UI/ObjectDisabler.cs
@@ -1,22 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
-using UnityEditor.UI;
+//using UnityEditor.UI;
 using UnityEngine;
 
 public class ObjectDisabler : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     public void DisableObject() {
         gameObject.SetActive(false);
     }
